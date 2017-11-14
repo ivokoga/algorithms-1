1cc3255e32d547531d4748f7c9f9c3995bd8bd54
Ivo Koga <ivo.koga@gmail.com>
2017-11-13 23:58:00 -0200

    is open implementation

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,7 +1,14 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
+    boolean[][] grid;
+    
     // create n-by-n grid, with all sites blocked
    public Percolation(int n) {
-       
+       grid = new boolean[n][n];
+//	for (int i = 0; i < N; i++) {
+//	    id[i] = i;
+//	}
    }
 // open site (row, col) if it is not open already
    public    void open(int row, int col) {
@@ -9,8 +16,9 @@ public class Percolation {
    }
 // is site (row, col) open?
    public boolean isOpen(int row, int col) {
-       return false;
+       return grid[row][col];
    }
+   
 // is site (row, col) full?
    public boolean isFull(int row, int col) {
        return false;
